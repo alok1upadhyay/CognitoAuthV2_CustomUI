87b9a99e3d068b619f193a5861f113d62ed90a3a
Alok Upadhyay <maddy.lucky4u@gmail.com>
2019-11-06 18:44:50 +0530

    AWSCognitoCustomSignUp
    This is a custom UI signIn, SignUp, ForgetPassword etc.

---
+++ Swift/CognitoYourUserPoolsSample/Constants.swift
@@ -0,0 +1,26 @@
+//
+// Copyright 2014-2018 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Amazon Software License (the "License").
+// You may not use this file except in compliance with the
+// License. A copy of the License is located at
+//
+//     http://aws.amazon.com/asl/
+//
+// or in the "license" file accompanying this file. This file is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, express or implied. See the License
+// for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = .USEast2
+let CognitoIdentityUserPoolId = "us-east-2_Y56E36yl0"
+let CognitoIdentityUserPoolAppClientId = "796h9gpfpn1889s8suo8jlplat"
+let CognitoIdentityUserPoolAppClientSecret = "vbahedq1sfpdlv0necm8i4p1cuk8t22bd61nid9d2nnmtnjhjc5"
+
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"
